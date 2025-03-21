2ef49e65c63ebbb47ee3738d02eb9f16e902e09e
zakgul-bbc <lara.akguen@gmail.com>
2025-03-21 19:37:46 +0100

    fixed blob issue

--- model/save.py
+++ model/save.py
@@ -17,7 +17,11 @@ except Exception:
 
 # Versionierung über aufsteigende Nummer
 blobs = blob_service_client.get_container_client(container_name).list_blobs()
-existing_versions = [int(blob.name.split('-')[-1].replace('.pkl', '')) for blob in blobs if blob.name.startswith("immoscout-model")]
+existing_versions = [
+    int(blob.name.split('-')[-1].replace('.pkl', ''))
+    for blob in blobs
+    if blob.name.startswith("immoscout-model") and blob.name.endswith(".pkl")
+]
 new_version = max(existing_versions, default=0) + 1
 
 # Upload des Modells
